93af6caff510d12ffa984ef583a6238e6b53c546
root <morganpsell@gmail.com>
2020-10-30 00:38:10 +0000

    Fixed users table bug. Data is now successfully extracted. Table is created.

--- sql_queries.py
+++ sql_queries.py
@@ -11,7 +11,7 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays (songplay_id INT PRIMARY KEY, start_time TIME NOT NULL, user_id INT, level VARCHAR, song_id VARCHAR, artist_id VARCHAR,
                             session_id INT, location VARCHAR, user_agent VARCHAR)""")
 
-user_table_create = ("""CREATE TABLE IF NOT EXISTS users (user_id INT PRIMARY KEY, title VARCHAR, artist_id INT, gender VARCHAR, level VARCHAR)""")
+user_table_create = ("""CREATE TABLE IF NOT EXISTS users (user_id INT PRIMARY KEY, first_name VARCHAR, last_name VARCHAR, gender VARCHAR, level VARCHAR)""")
 
 song_table_create = ("""CREATE TABLE IF NOT EXISTS songs (song_id VARCHAR PRIMARY KEY, title VARCHAR, artist_id VARCHAR, year INT, duration NUMERIC)""")
 
@@ -24,11 +24,13 @@ time_table_create = ("""CREATE TABLE IF NOT EXISTS time (start_time TIME NOT NUL
 songplay_table_insert = ("""INSERT INTO songplays (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
                             VALUES (%s, %s, %s, %s, %s, %s, %s);""")
 
-user_table_insert = ("""INSERT INTO users (user_id, title, artist_id, gender, level) VALUES (%s, %s, %s, %s, %s);""")
+user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s)
+                        ON CONFLICT(user_id) DO NOTHING;""")
 
 song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s);""")
 
-artist_table_insert = ("""INSERT INTO artists (artist_id, name, location, latitude, longitude) VALUES (%s, %s, %s, %s, %s);""")
+artist_table_insert = ("""INSERT INTO artists (artist_id, name, location, latitude, longitude) VALUES (%s, %s, %s, %s, %s)
+                          ON CONFLICT(artist_id) DO NOTHING;""")
 
 
 time_table_insert = ("""INSERT INTO time (start_time, hour, day, week, month, year, weekday) VALUES (%s, %s, %s, %s, %s, %s, %s);""")
